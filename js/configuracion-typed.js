d619ca4d0bcaa3d6432a8b69278bfd3b2215d046
agl5702 <advasquez41@misena.edu.co>
2023-08-17 19:54:30 -0500

    3r commit proyect

--- js/configuracion-typed.js
+++ js/configuracion-typed.js
@@ -1,13 +1,11 @@
-const typed = new Typed('.typed', {
+var typed = new Typed('.typed', {
     strings: [
-        '<i class="deporte">Fútbol</i>',
-        '<i class="deporte">Basquetbol</i>',
-        '<i class="deporte">Voleibol</i>'
+
     ],
     stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-    typeSpeed: 85, // Velocidad en mlisegundos para poner una letra,
+    typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
     startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-    backSpeed: 85, // Velocidad en milisegundos para borrrar una letra,
+    backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
     smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
     shuffle: false, // Alterar el orden en el que escribe las palabras.
     backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
@@ -15,5 +13,5 @@ const typed = new Typed('.typed', {
     loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
     showCursor: true, // Mostrar cursor palpitanto
     cursorChar: '|', // Caracter para el cursor
-    contentType: 'html',
+    contentType: 'html', // 'html' o 'null' para texto sin formato
 });
